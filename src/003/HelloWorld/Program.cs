7b355ffb4e09a9464bac53973c1b7f58673e4be1
Dan Gilleland <dagilleland@shaw.ca>
2024-01-12 14:49:54 -0700

    Lesson 003

---
+++ src/003/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+namespace App;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+        if (args.Length == 0)
+            WriteLine("\tNo information was passed to this program.");
+        else
+            foreach (string info in args)
+                WriteLine($"\t{info}");
+
+    }
+}
