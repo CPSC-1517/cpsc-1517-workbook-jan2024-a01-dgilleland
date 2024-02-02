fc314ca76bf3b89bc57ac8d4e813fbe474aa6965
Dan Gilleland <dagilleland@shaw.ca>
2024-02-02 13:49:49 -0700

    WIP: 005 - More ad-hoc code samples, with `List<T>` usage

---
+++ src/005/MoreCode/MultipleChoice.cs
@@ -0,0 +1,73 @@
+﻿namespace Assorted;
+
+/// <summary>
+/// Represent a selection of an option for a multiple-choice question
+/// </summary>
+public class MultipleChoice
+{
+    /// <summary>
+    /// Represents a possible answer for a multiple-choice question (A through E)
+    /// </summary>
+    public enum Answer { NOT_ANSWERED, A, B, C, D, E }
+
+    public Answer Choice { get; }
+
+    public MultipleChoice()
+    {
+        Choice = Answer.NOT_ANSWERED;
+    }
+
+    public MultipleChoice(Answer chosenAnswer)
+    {
+        Choice = chosenAnswer;
+    }
+}
+
+/// <summary>
+/// Represents a Mark on a assignment or quiz
+/// </summary>
+public class Mark
+{
+    public int Possible { get; }
+    public int Earned { get; }
+    public double Average { get { return 100.0 * Earned / Possible; } }
+
+    public Mark(int possible, int earned)
+    {
+        // TODO: Validation
+        Possible = possible;
+        Earned = earned;
+    }
+}
+
+
+public class MultipleChoiceMarker
+{
+    public List<MultipleChoice> Key { get; }
+    public int Count { get; }
+
+    public MultipleChoiceMarker(List<MultipleChoice> key)
+    {
+        // TODO: Validation
+        Key = key;
+    }
+
+    public Mark MarkExam(List<MultipleChoice> studentAnswers)
+    {
+        // TODO: Validation
+        // Loop through the student answers and compare them against the answer key
+        int correct = 0;
+        for(int index = 0; index < Key.Count; index++)
+        {
+            var keyItem = Key[index];
+            var student = studentAnswers[index];
+            if(student.Choice != MultipleChoice.Answer.NOT_ANSWERED)
+            {
+                if(keyItem.Choice == student.Choice)
+                    correct++;
+            }
+        }
+
+        return new Mark(Key.Count, correct);
+    }
+}
---
+++ src/005/MoreCode/PhoneNumber.cs
@@ -0,0 +1,48 @@
+﻿namespace Assorted;
+
+public class PhoneNumber
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Number { get; private set; }
+
+    public PhoneNumber(string first, string last, string number)
+    {
+        FirstName = first;
+        LastName = last;
+        Number = number;
+    }
+}
+
+public class PhoneBook
+{
+    public List<PhoneNumber> Numbers { get; }
+    public string Name { get; }
+    public PhoneBook(string areaName)
+    {
+        Name = areaName;
+        Numbers = new(); // empty list
+    }
+
+    public void Add(PhoneNumber entry)
+    {
+        // TODO: validation (e.g.: entry should not be null)
+        Numbers.Add(entry); // appending, without consideration of maintaining a sort order.
+    }
+
+    public List<PhoneNumber> Find(string partialName)
+    {
+        // TODO: implement this later
+        throw new NotImplementedException();
+    }
+
+    public PhoneNumber FindByNumber(string number)
+    {
+        PhoneNumber found = null;
+        // Simple sequential search
+        foreach(PhoneNumber entry in Numbers)
+            if(entry.Number == number)
+                found = entry;
+        return found;       
+    }
+}
