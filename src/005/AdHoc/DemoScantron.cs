0f32165e58e9fdad0898a5f9f8b918c18d95fade
Dan Gilleland <dagilleland@shaw.ca>
2024-02-02 14:37:40 -0700

    WIP: 005 - Scantron

---
+++ src/005/AdHoc/DemoScantron.cs
@@ -0,0 +1,9 @@
+﻿namespace AdHoc;
+
+public class DemoScantron
+{
+    public static void Run(string[] args)
+    {
+        // Load up a file of exam answers which can then be parsed and marked
+    }
+}
